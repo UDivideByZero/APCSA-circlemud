0b79e86900a87eb9485d9d7276f9bda6c946fdcd
THEGavinKrause <gavinmicahkrause@gmail.com>
2026-04-07 18:21:22 +0000

    remove offensive message about mother

--- src/act.social.c
+++ src/act.social.c
@@ -155,7 +155,7 @@ ACMD(do_insult)
 	  }
 	  break;
 	case 1:
-	  act("$n calls your mother a bitch!", FALSE, ch, 0, victim, TO_VICT);
+	  act("$n calls your mother a lovely women!", FALSE, ch, 0, victim, TO_VICT);
 	  break;
 	default:
 	  act("$n tells you to get lost!", FALSE, ch, 0, victim, TO_VICT);
